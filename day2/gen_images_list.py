cbcb2ff1147505819b8df9191c5978971cde1233
burness <dss_1990@sina.com>
2016-11-14 20:55:13 +0800

    update code for im2rec

--- day2/gen_images_list.py
+++ day2/gen_images_list.py
@@ -11,7 +11,7 @@ def gen_images_list(data_dir='17flowers/', test_spilt =0.3):
     for sub_dir in sub_dirs:
         c_dir = os.path.join(data_dir, sub_dir)
         img_label = sub_dir
-        c_dir_files = [os.path.join(c_dir,f) for f in os.listdir(c_dir) if os.path.isfile(os.path.abspath(os.path.join(c_dir,f)))]
+        c_dir_files = [os.path.join(sub_dir,f) for f in os.listdir(c_dir) if os.path.isfile(os.path.abspath(os.path.join(c_dir,f)))]
         c_dir_files_len = len(c_dir_files)
         c_dir_files_test_len = int(test_spilt*c_dir_files_len)
         random.shuffle(c_dir_files)
